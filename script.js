5ea2bbb0f3ddffe2c2258ce38e4fd318c056dbba
Lucy Ifeneme <lucyifeneme@gmail.com>
2022-03-07 16:28:53 +0100

    add event listener

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let addToDoButton = document.getElementById('addToDo');
 let toDOcontainer = document.getElementById('toDocontainer');
 let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p')
+    paragraph.innerText = inputField.value;
+    toDOcontainer.appendChild(paragraph);
+})
